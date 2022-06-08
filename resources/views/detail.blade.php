4c861e3d674c5dc7a44ea9c05d8aaf687cf553e4
Abenathi <cwatiabenathi@gmail.com>
2022-06-08 03:33:27 -0700

    add to cart function completed

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -13,7 +13,12 @@
            <h3>Price: {{$product['price']}}</h3>
            <h4>{{$product['description']}}</h4>
            <h5>{{$product['category']}}</h5>
-           <button class="btn btn-primary">Add to Cart</button>
+           <form action="/add_to_cart" method="post">
+             @csrf
+             <input type="text" name="product_id" value="{{$product['id']}}" hidden>
+             <button class="btn btn-primary">Add to Cart</button>
+           </form>
+           
            <button class="btn btn-success">Buy Now</button>
        </div>
     </div>
